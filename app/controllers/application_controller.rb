c298b700bf83c5ea076f98f007a66b43b4b220a5
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-07-17 18:14:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+  get "/:number1/:number2" do
+    @num1 = params[:number1].to_i 
+    @num2 = params[:number2].to_i 
+    # result = @num1 * @num2
+    # # puts "#{result}"
+    "#{@num1 * @num2}"
+    # "#{result}"
+  end
 end
